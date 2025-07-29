161f949374a1df72ad435455b99f39f38306cbee
Eugene Maslovich <ehpc@ehpc.io>
2025-07-29 15:00:37 +0000

    Remove outdated tasks and code implementations from days 6 to 9, including skills sorting, tree traversal, and debounce functions. Add new tasks for days 10 and 3, focusing on range reduction, async semaphore, and binary search with callbacks. Implement LRU cache and string compression functionalities.

--- day_8/1.js
+++
@@ -1,7 +0,0 @@
-console.log(diff([1, 2, 3, 4, 5], [3, 4, 1])); // [2, 5]
-
-function diff(firstArray, secondArray) {
-  const set = new Set(secondArray);
-
-  return firstArray.filter((el) => !set.has(el));
-}
