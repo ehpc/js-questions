45e1fd7adcd6daa890632a30a158dea2cc261bf3
Eugene Maslovich <ehpc@ehpc.io>
2025-07-30 08:31:20 +0000

    Initialize project with TypeScript configuration, package.json, and .gitignore files

---
+++ day_1/day_1_2.ts
@@ -0,0 +1,46 @@
+class Parent {
+  foo() {
+    console.log('It works!');
+  }
+}
+
+class Example extends Parent {}
+
+// Your code starts here
+
+function partial(target, obj) {
+  const parentPrototype = Object.getPrototypeOf(target.prototype);
+  Object.setPrototypeOf(obj, parentPrototype);
+  Object.defineProperties(target.prototype, Object.getOwnPropertyDescriptors(obj))
+}
+
+/*
+Важный нюанс! Super работает через [[HomeObject]] (10.2 ECMAScript Function Objects).
+При этом изменить [[HomeObject]] мы не можем, и, соответственно, мы вынуждены менять
+прототип именно у obj, а не его копии, например. Потому что, если бы мы решили сделать
+копию obj, то при копировании в неё методов, [[HomeObject]] бы всё равно указывал на obj.
+*/
+
+// Your code ends here
+
+partial(Example, {
+  foo() {
+    super.foo();
+    console.log('Yeaaah');
+  },
+  
+  get bar() {
+    return Math.random();
+  }
+});
+
+const example = new Example();
+
+example.foo(); // It works! Yeaaah
+
+// @ts-ignore
+console.log(example.bar); // Случайное число
+// @ts-ignore
+console.log(example.bar); // Случайное число
+// @ts-ignore
+console.log(example.bar); // Случайное число
